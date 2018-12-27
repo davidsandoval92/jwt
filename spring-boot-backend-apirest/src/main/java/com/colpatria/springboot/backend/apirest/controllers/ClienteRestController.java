49527b1981faec9f4d711833cfcc51cd4c748cf1
David Sandoval <davidsandoval9217@gmail.com>
2018-12-27 14:55:17 -0500

    david sandoval: implementacion cors filters y terminacion de servidore de autenticacion y recursos (JWT y OAuth2)

--- spring-boot-backend-apirest/src/main/java/com/colpatria/springboot/backend/apirest/controllers/ClienteRestController.java
+++ spring-boot-backend-apirest/src/main/java/com/colpatria/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +27,7 @@ public class ClienteRestController {
 
 	}
 	
+	@Secured({"ROLE_ADMIN"})
 	@GetMapping("/clientes/{id}")
 	public ResponseEntity<?> show(@PathVariable Long id) {
 		
