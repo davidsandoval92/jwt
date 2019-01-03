79fac32e3d945ec1fba82ad4c4cb4496da74ec96
David Sandoval <davidsandoval9217@gmail.com>
2019-01-03 16:09:35 -0500

    import librerias

--- spring-boot-backend-apirest/src/test/java/com/colpatria/springboot/backend/apirest/controllers/ClienteRestControllerTest.java
+++ spring-boot-backend-apirest/src/test/java/com/colpatria/springboot/backend/apirest/controllers/ClienteRestControllerTest.java
@@ -1,8 +1,10 @@
 package com.colpatria.springboot.backend.apirest.controllers;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
