d8d309fafd7dc0f723280403004ef3823daeccf3
Franklin carrero <franklin.carrero@ceiba.com.co>
2019-01-03 10:14:26 -0500

    JUMP:00 change database to h2 for test

--- spring-boot-backend-apirest/src/test/java/com/colpatria/springboot/backend/apirest/controllers/ClienteRestControllerTest.java
+++ spring-boot-backend-apirest/src/test/java/com/colpatria/springboot/backend/apirest/controllers/ClienteRestControllerTest.java
@@ -6,7 +6,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-
+import static org.hamcrest.Matchers.*;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -27,6 +28,7 @@ import com.colpatria.springboot.backend.apirest.SpringBootBackendApirestApplicat
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = SpringBootBackendApirestApplication.class)
 @AutoConfigureMockMvc
+@TestPropertySource( locations = "classpath:applicationtest.properties")
 public class ClienteRestControllerTest {
 
 	@Autowired
@@ -81,6 +83,7 @@ public class ClienteRestControllerTest {
                 .contentType(CONTENT_TYPE)              
                 .accept(CONTENT_TYPE))      
         		.andDo(print())
-                .andExpect(status().isOk());   
+        		.andExpect(jsonPath("$", hasSize(12)))
+                .andExpect(status().isOk());  
     }
 }
